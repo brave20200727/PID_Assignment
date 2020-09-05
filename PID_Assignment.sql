1f9cb3029f51743d520e39db4e0e559ccb63c318
Brave <brave_chen@mail.chungyo.net>
2020-09-05 17:16:40 +0800

    輸出資料庫

---
+++ PID_Assignment.sql
@@ -0,0 +1,186 @@
+-- MySQL dump 10.13  Distrib 5.7.26, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: PID_Assignment
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cart` (
+  `userId` int(11) DEFAULT NULL,
+  `productId` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orderDetails`
+--
+
+DROP TABLE IF EXISTS `orderDetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orderDetails` (
+  `orderId` int(11) DEFAULT NULL,
+  `productId` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderDetails`
+--
+
+LOCK TABLES `orderDetails` WRITE;
+/*!40000 ALTER TABLE `orderDetails` DISABLE KEYS */;
+INSERT INTO `orderDetails` VALUES (1,1,10),(1,2,10),(1,3,10),(2,1,10),(2,2,10),(2,3,10),(3,1,10),(3,2,10),(3,3,10),(4,1,10),(4,2,10),(4,3,10);
+/*!40000 ALTER TABLE `orderDetails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `orderId` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) DEFAULT NULL,
+  `shippingStatus` int(11) DEFAULT NULL,
+  PRIMARY KEY (`orderId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,2,1),(2,2,1),(3,2,1),(4,2,1);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productTypes`
+--
+
+DROP TABLE IF EXISTS `productTypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productTypes` (
+  `typeId` int(11) NOT NULL AUTO_INCREMENT,
+  `typeName` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`typeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productTypes`
+--
+
+LOCK TABLES `productTypes` WRITE;
+/*!40000 ALTER TABLE `productTypes` DISABLE KEYS */;
+INSERT INTO `productTypes` VALUES (1,'電子書'),(2,'繁體中文書'),(3,'簡體中文書'),(4,'外文書'),(5,'雜誌'),(6,'漫畫'),(7,'文具用品');
+/*!40000 ALTER TABLE `productTypes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `productId` int(11) NOT NULL AUTO_INCREMENT,
+  `productName` varchar(20) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `productType` int(11) DEFAULT NULL,
+  `productPic` varchar(20) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `inStock` int(11) DEFAULT NULL,
+  PRIMARY KEY (`productId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'大肌肌維他命1','大肌肌維他命1',NULL,'img/1.jpg',1000,999),(2,'大肌肌維他命2','大肌肌維他命2',NULL,'img/1.jpg',2000,999),(3,'大肌肌維他命3','大肌肌維他命3',NULL,'img/1.jpg',3000,999);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(32) DEFAULT NULL,
+  `userPassword` varchar(64) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `gender` int(11) DEFAULT NULL,
+  `name` varchar(8) DEFAULT NULL,
+  `phone` varchar(16) DEFAULT NULL,
+  `city` varchar(8) DEFAULT NULL,
+  `address` varchar(64) DEFAULT NULL,
+  `userType` int(11) DEFAULT NULL,
+  `userStatus` int(11) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','admin',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,1),(2,'brave20200727','1234','brave20200727@gmail.com','1997-07-30',1,'陳柏程','0926005788','台南市','安平那兒',1,1),(3,'owen5566','1234','owen5566@gmail.com','1997-03-03',2,'黃煒翔','0923617373','台中市','鼎盛大樓',1,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-05 17:16:01
